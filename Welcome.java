f7263a4280789ec13cb59d571b04bd5af691ff7e
WarCrush <36442997+WarCrush@users.noreply.github.com>
2018-02-13 12:29:38 +0000

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -20,21 +20,30 @@ public class Graph {
 	int val = 0;
 
     for(int counter = 1; counter <= 5; counter++){
-	 if(counter == 1)
-	  val = n1;
-	 else if (counter == 2)
-	  val = n2;
-	 else if (counter == 3)
-	  val = n3;
-	 else if (counter == 4)
-	  val = n4;
-	 else if (counter == 5)
-	  val = n5;
-	for(int j = 1; j <= val; j++){
-      System.out.print("*");
+		switch(counter){
+			case 1:
+			 val = n1;
+			 break;
+			case 2:
+			 val = n2;
+			 break;
+			case 3:
+			 val = n3;
+			 break;
+			case 4:
+			 val = n4;
+			 break;
+			case 5:
+			 val = n5;
+			 break;
+			default:
+				System.out.println("There is a problem! !");
+			}
+			for(int j = 1; j <= val; j++)
+				System.out.print("*");
 
-	      }
-	      System.out.println();
-    }
-  }
+			System.out.println();
+		}
+
+}
 }
